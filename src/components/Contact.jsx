54988c9cf0f57443f2e857386753640c475b8841
Anushka Patil <anushkaa2704@gmail.com>
2025-06-28 19:33:15 +0530

    Changed contact number

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,7 @@ function Contact() {
 
       <div className="contact-info">
         <p><i className='bx bx-envelope'></i> anushkaa2704@gmail.com</p>
-        <p><i className='bx bx-phone'></i> +91-9876543210</p>
+        <p><i className='bx bx-phone'></i> +91-8779704743</p>
         <div className="social-icons">
           <a href="https://github.com/anushkaa2704" target="_blank" rel="noreferrer"><i className='bx bxl-github'></i></a>
           <a href="https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAAFZT86gBVCRqRmfaE2i8BB_gaql6vTXlK9s&keywords=anushka%20patil&origin=RICH_QUERY_SUGGESTION&position=0&searchId=aee74a71-2348-4084-9360-4b8b167ad732&sid=ba%40&spellCorrectionEnabled=false" target="_blank" rel="noreferrer"><i className='bx bxl-linkedin'></i></a>
